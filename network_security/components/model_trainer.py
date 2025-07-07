d093ab74583b59579b88b4b7f060a5aa09ef0a8f
PHANUWAT\panuw <Panuwat_nine@hotmail.com>
2025-07-07 21:52:21 +0700

    mlflow commited

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -31,6 +31,9 @@ from sklearn.ensemble import (
     AdaBoostClassifier,
 )
 
+import mlflow
+import mlflow.sklearn
+
 
 class ModelTrainer:
     def __init__(
@@ -44,6 +47,17 @@ class ModelTrainer:
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+    def track_mlflow(self, best_model, classification_metric):
+        with mlflow.start_run():
+            f1_score = classification_metric.f1_score
+            precision_score = classification_metric.precision_score
+            recall_score = classification_metric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(sk_model=best_model, artifact_path="model")
 
     def train_model(self, X_train, y_train, x_test, y_test) -> ModelTrainerArtifact:
         models = {
@@ -104,12 +118,14 @@ class ModelTrainer:
             y_true=y_train, y_pred=y_train_pred
         )
 
-        ## Track the experiments with mlflow (TODO: Implement MLflow tracking)
+        self.track_mlflow(best_model, classification_train_metric)
 
         y_test_pred = best_model.predict(x_test)
         classification_test_metric = get_classification_score(
             y_true=y_test, y_pred=y_test_pred
         )
+
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(
             file_path=self.data_transformation_artifact.transformed_object_file_path
