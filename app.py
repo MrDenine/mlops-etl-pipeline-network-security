a7a27196538de9501a27b6212df37ec36f0b1214
PHANUWAT\panuw <Panuwat_nine@hotmail.com>
2025-07-08 21:59:25 +0700

    pipeline complete

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile, Request
 from uvicorn import run as app_run
 from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 import pandas as pd
 
@@ -20,6 +21,7 @@ from network_security.constants.training_pipeline import (
     DATA_INGESTION_COLLECTION_NAME,
     DATA_INGESTION_DATABASE_NAME,
 )
+from network_security.utils.ml_utils.model.estimator import NetworkModel
 
 ca = certifi.where()
 
@@ -45,18 +47,55 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+templates = Jinja2Templates(directory="./templates")
+
 
 @app.get("/", tags=["Authentication"])
 async def index():
     return RedirectResponse(url="/docs")
 
 
-@app.get("/train")
-async def train():
+@app.get("/train", tags=["Training"])
+async def train_route():
     try:
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
         return Response("Training pipeline executed successfully.", status_code=200)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+@app.post("/predict", tags=["Prediction"])
+async def predict_route(request: Request, file: UploadFile = File(...)):
+    try:
+        if file.filename.endswith(".csv"):
+            df = pd.read_csv(file.file)
+        elif file.filename.endswith(".xlsx"):
+            df = pd.read_excel(file.file)
+        else:
+            return Response(
+                "Unsupported file format. Please upload a CSV or Excel file.",
+                status_code=400,
+            )
+
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor, final_model)
+
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df["predicted_column"] = y_pred
+        print(df["predicted_column"])
+
+        df.to_csv("prediction_output/predictions.csv")
+
+        table_html = df.to_html(classes="table table-striped")
+
+        return templates.TemplateResponse(
+            "table.html", {"request": request, "table": table_html}
+        )
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
